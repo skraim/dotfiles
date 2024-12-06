ac8e415957b4cd6de5f465855ce0719c769b4002
Artem <darkskraim@gmail.com>
2024-12-06 23:50:05 +0200

    remove bspwm configurations

--- betterlockscreen/.config/betterlockscreen/custom-pre.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-xkb-switch -s us &
-playerctl pause &
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause &
--- polybar/.config/polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q polybar
-
-sleep 1;
-polybar --reload main &
-polybar --reload small &
